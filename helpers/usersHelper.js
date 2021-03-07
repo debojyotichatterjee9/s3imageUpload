b4561cc5df87c9c955b216909c8d26a58fd342d2
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2021-03-08 02:00:59 +0530

    updated readme

--- helpers/usersHelper.js
+++ helpers/usersHelper.js
@@ -1,49 +1,52 @@
 
+const { v4: uuidv4 } = require('uuid');
 const imageUploadServiceObj = require('../utils/imageUploadService')
 
 exports.uploadUserAvatar = async (userId, payload) => {
     try {
 
-        
         if (payload.hasOwnProperty("image")) {
             const base64Image = payload.image;
             const imageCategory = 'avatar';
-
+            const prevImage = uuidv4().replace(/[ -]/g, '');
             const params = {
                 userId,
                 base64Image,
+                prevImage,
                 imageCategory
             }
+            // creating an object for custom imageUploadService
             let imageServiceObj = new imageUploadServiceObj.ImageService(params);
+            // checking if the string in the payload is in valid base64 format.
             if (!imageServiceObj.isValidBase64()) {
-
                 return ({
                     message: 'Supplied image is not in base64 format.'
                 })
             }
+            // checking if file size is more than a specified size.
             else if (imageServiceObj.isGreaterThan(5)) { //5 MB
-
                 return ({
                     message: 'Supplied image is greater than 5 MB.'
                 })
             }
+            // checking if the file is of valid type
             else if (!imageServiceObj.isValidImageType()) {
                 return ({
                     message: 'Supplied image type is invalid.'
                 })
             }
             else {
-
                 const amazonResponse = await imageServiceObj.uploadToS3Bucket();
-
+                // if the response from aws is correct return the data
                 if (amazonResponse.hasOwnProperty('eTag') && amazonResponse.hasOwnProperty('location')) {
                     const fileLocation = `${amazonResponse.location}`
                     return ({
-                    	id: userId,
+                        id: userId,
                         location: fileLocation
                     });
                 }
                 else {
+                    // else return error with message
                     return ({
                         ref: 'UPLOAD_FAILED',
                         message: amazonResponse.message
@@ -54,7 +57,6 @@ exports.uploadUserAvatar = async (userId, payload) => {
         else {
             return (false);
         }
-
     }
     catch (err) {
         return {
--- routes/users.js
+++ routes/users.js
@@ -6,21 +6,25 @@ const { v4: uuidv4 } = require('uuid');
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
 });
 
 
 /* Upload image to S3. */
 router.post('/image/upload', async (req, res, next) => {
-	const [ payload ] = [ req.body ];
-	if (Object.keys(payload).length === 0) {
+    const [payload] = [req.body];
+
+    // throw error on blank payload
+    if (Object.keys(payload).length === 0) {
         return res.status(400).send({
             error: {
                 message: 'Blank payload supplied.'
             }
         });
     }
+
+    // throw error on wrong payload
     if ((!payload.hasOwnProperty('image')) || (payload.image == '')) {
         return res.status(400).send({
             error: {
@@ -30,23 +34,23 @@ router.post('/image/upload', async (req, res, next) => {
     }
 
     if (payload) {
-    			const user_id = uuidv4(); //generating a random user_id
-                const uploadedAvatarResponse = await usersHelperObj.uploadUserAvatar(user_id, payload);
-                if (uploadedAvatarResponse.hasOwnProperty('id') && uploadedAvatarResponse.hasOwnProperty('location')) {
-                    res.status(200).send(uploadedAvatarResponse);
-                }
-                else {
-                    res.status(400).send({ error: uploadedAvatarResponse });
-                }
+        const user_id = uuidv4(); //generating a random user_id
+        const uploadedAvatarResponse = await usersHelperObj.uploadUserAvatar(user_id, payload);
+        // check if the response is correct
+        if (uploadedAvatarResponse.hasOwnProperty('id') && uploadedAvatarResponse.hasOwnProperty('location')) {
+            res.status(200).send(uploadedAvatarResponse);
+        }
+        else {
+            res.status(400).send({ error: uploadedAvatarResponse });
+        }
+    }
+    else {
+        return res.status(400).send({
+            error: {
+                message: "Bad Request."
             }
-            else {
-                return res.status(403).send({
-                    error: {
-                        message: "Bad Request."
-                    }
-                });
-            }
-  // res.send('upload image to s3');
+        });
+    }
 });
 
 
