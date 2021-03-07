7ac5b6caf6d7665ab986a4f19103eb58c476fcae
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2021-03-07 21:00:52 +0530

    first draft -- added upload function

--- app.js
+++ app.js
@@ -4,17 +4,25 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const config = require('config');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// for allowing larger payloads to process
+const bodyParser = require('body-parser');
+app.use(bodyParser.json({ limit: config.get('request_body').max_size_allowed }))
+app.use(bodyParser.urlencoded({limit: config.get('request_body').max_size_allowed, extended: true, parameterLimit: 50000}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
+// app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
